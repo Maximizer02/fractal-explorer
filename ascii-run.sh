5510b120b172239b688d4c4280bc2bf7f2310e25
Maximizer02 <max.beller@yahoo.com>
2025-10-15 16:04:54 +0200

    out directory

--- ascii-run.sh
+++ ascii-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc mandelbrot.c mandelbrot.h ascii.c -o ascii -lm -O3 && ./ascii;
+gcc mandelbrot.c mandelbrot.h ascii.c -o out/ascii -lm -O3 && out/./ascii;
--- ppm-run.sh
+++ ppm-run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gcc mandelbrot.c mandelbrot.h ppm.c -o ppm -lm -O3 && ./ppm && ffplay mandelbrot.ppm;
+gcc mandelbrot.c mandelbrot.h ppm.c -o out/ppm -lm -O3 \
+	&& out/./ppm \
+	&& mv mandelbrot.ppm out/ \
+	&& ffplay out/mandelbrot.ppm;
